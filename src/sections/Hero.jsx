6cfcef12101e310f51a1e30c487b0d4ade1e5e13
Lenny Wandeto <161931642+LennyWandeto@users.noreply.github.com>
2025-04-29 01:03:23 -0400

    updates for size

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                         <PerspectiveCamera makeDefault position={isMobile ? [0, 0, 40] : [0, 0, 30]} />
                         <HeroCamera>
                             <HackerRoom
-                                scale={isMobile ? 8.5 : 7}   // smaller on mobile
+                                scale={isMobile ? 8 : 7}   // smaller on mobile
                                 position={[0, 0, 0]}
                             />
                         </HeroCamera>
@@ -37,20 +37,20 @@ const Hero = () => {
                             <Suspense fallback={<Socrates url='/models/bust-lo-draco.glb'
                                 position={isMobile ? [-7.7, 0, -1] : [-14, -9.5, 2.9]}
                                 rotation={[-0.1, -0.9, 0.06]}
-                                scale={isMobile ? 1.6 : 1} />}>
+                                scale={isMobile ? 1.2 : 1} />}>
 
                                 <Socrates
                                     url='/models/bust-hi.glb'
                                     position={isMobile ? [-7.7, 0, -1] : [-14, -9.5, 2.9]}
                                     rotation={[-0.1, -0.9, 0.06]}
-                                    scale={isMobile ? 1.5 : 1}
+                                    scale={isMobile ? 1.2 : 1}
                                 />
                             </Suspense>
                         </group>
 
                         <group>
                             <Torus
-                                scale={isMobile ? 0.9 : 1.4}
+                                scale={isMobile ? 0.7 : 1.4}
                                 position={isMobile ? [3.3, -4.4, 22] : [12.9, 1.3, 5.1]}
                             />
                         </group>
